0183b0a245549034e3c4fb4efed12e59a8d7881e
Quentin <quentin@bernigaud.fr>
2020-01-02 18:21:47 +0100

    TD1-7 Navigation

--- app/src/main/java/com/android/example/recyclerview/AuthenticationFragment.kt
+++ app/src/main/java/com/android/example/recyclerview/AuthenticationFragment.kt
@@ -1,21 +1,18 @@
 package com.android.example.recyclerview
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.authentication_fragment.*
 
 class AuthenticationFragment: Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        login.setOnClickListener {
-            findNavController().navigate(R.id.action_authenticationFragment_to_loginFragment)
-        }
-
-        signup.setOnClickListener {
-            findNavController().navigate(R.id.action_authenticationFragment_to_signupFragment)
-        }
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        login.setOnClickListener {
+//            findNavController().navigate(R.id.action_authenticationFragment_to_loginFragment)
+//        }
+//
+//        signup.setOnClickListener {
+//            findNavController().navigate(R.id.action_authenticationFragment_to_signupFragment)
+//        }
+//    }
 
 }
